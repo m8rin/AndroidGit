40e85598f80851c25eefd428a33b902634bab302
m8rin <rinatrayan@mail.ru>
2021-04-01 17:02:51 +0500

    feature(labs): added FirebaseApplication

---
+++ lab8/FirebaseApplication/app/src/main/java/isangulov/projects/firebaseapplication/MainActivity.java
@@ -0,0 +1,88 @@
+package isangulov.projects.firebaseapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText ETemail;
+    private EditText ETpassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+
+                } else {
+                    // User is signed out
+                }
+            }
+        };
+
+        ETemail = findViewById(R.id.editTextMail);
+        ETpassword =  findViewById(R.id.editTextPassword);
+
+        findViewById(R.id.btn_sign_in).setOnClickListener(this);
+        findViewById(R.id.btn_registration).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btn_sign_in)
+        {
+            signin(ETemail.getText().toString(),ETpassword.getText().toString());
+        }else if (view.getId() == R.id.btn_registration)
+        {
+            registration(ETemail.getText().toString(),ETpassword.getText().toString());
+        }
+    }
+
+    public void signin(String email , String password)
+    {
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Авторизация прошла успешна", Toast.LENGTH_SHORT).show();
+                }else
+                    Toast.makeText(getApplicationContext(), "Не удалось авторизоваться", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void registration (String email , String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(getApplicationContext(), "Регистрация прошла успешна", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "Не удалось зарегистрироваться", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
